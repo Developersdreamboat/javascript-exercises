9c550ce59ea8365e5484ef453cc2afed4c4bbd0c
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2022-12-11 17:31:24 +0200

    Add solution for reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = (string) => {
+  let resultString = '';
+  for (let i = 0; i <= string.length; i++) {
+    resultString += string.charAt(string.length - i);
+  }
+  return resultString;
 };
 
 // Do not edit below this line
