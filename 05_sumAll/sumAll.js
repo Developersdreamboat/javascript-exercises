ff2a4f00b07d13eaed2e16ba62fa460cfb3df16e
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2022-12-11 20:44:25 +0200

    Add solution for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = (x, y) => {
+  let resultValue = 0;
+  let biggerNumber;
+  let lesserNumber;
 
+  if ((typeof(x) != "number") || 
+      (typeof(y) != "number") || 
+       x < 0 || 
+       y < 0) {
+    return "ERROR";
+  }
+
+  if (y > x) {
+    biggerNumber = y;
+    lesserNumber = x;
+  } else {
+    biggerNumber = x;
+    lesserNumber = y;
+  }
+
+  for (let i = 0; i <= (biggerNumber - lesserNumber); i++) {
+    resultValue += (lesserNumber + i);
+  }
+  
+  return resultValue;
 };
 
 // Do not edit below this line
