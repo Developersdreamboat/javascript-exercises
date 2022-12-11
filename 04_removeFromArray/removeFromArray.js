78fbbb8b47bb8b1953a71ead7ffa24b91492422d
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2022-12-11 19:51:55 +0200

    Add solution for removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = (array, ...numbers) => {
+  let resultArray = array;
+  for (const arg of numbers) {
+    for (let i = 0; i <= resultArray.length; i++) {
+      if (resultArray[i] === arg) {
+        resultArray.splice(i, 1);
+      }
+    }
+  }
+  return resultArray;
 };
 
 // Do not edit below this line
