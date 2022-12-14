746fb7d34b80c418072611bb72c79fa2d1ed4c4a
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2022-12-14 17:55:06 +0200

    Add solution for findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  const max = array.reduce(function(prev, current) {
+      prevAge = calculateAge(prev);
+      currentAge = calculateAge(current);
+      return (prevAge > currentAge) ? prev : current
+  }) 
+  return max;
 };
+
+const calculateAge = function(object) {
+  let age;
+  if (!object.yearOfDeath) {
+    age = new Date().getFullYear() - object.yearOfBirth;
+  } else {
+    age = object.yearOfDeath - object.yearOfBirth;
+  }
+  return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
