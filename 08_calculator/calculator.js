cedb5128b750f710b7750f310d2f95e5a13d0a53
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2022-12-14 12:22:15 +0200

    Add solution for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return +a + +b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return +a - +b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let resultSum = 0;
+	for (let i = 0; i < array.length; i++) {
+    resultSum += array[i];
+  }
+  return resultSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let resultMultiply = 1;
+	for (let i = 0; i < array.length; i++) {
+    resultMultiply *= array[i];
+  }
+  return resultMultiply;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let resultOfPower = 1;
+	for (let i = 0; i < exponent; i++) {
+    resultOfPower *= base;
+  }
+  return resultOfPower;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let resultOfFactorial = 1;
+  for (let i = 0; i < number; i++) {
+    resultOfFactorial *= number - i;
+  }
+  return resultOfFactorial;
 };
 
 // Do not edit below this line
