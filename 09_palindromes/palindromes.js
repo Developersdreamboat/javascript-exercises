8ca1211647b0e76a01617ec3db58cf339bd9a914
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2022-12-14 14:02:04 +0200

    Add solution for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let stringFromLeft = string.toLowerCase().replace(/[^a-z]/g, "");
+  let stringFromRight = stringFromLeft.split("").reverse().join("");
+  
+  if (stringFromLeft == stringFromRight) {
+    return true
+  } else return false;
 };
 
 // Do not edit below this line
