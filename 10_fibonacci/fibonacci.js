9a864f9eb66edbd347374d1f5fe487bdb95e9218
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2022-12-14 15:18:51 +0200

    Add solution for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  
+  if (number < 1) {
+    return "OOPS";
+  }
+  let array = [0, 1];
+  
+  for (let i = 1; i < number; i++) {
+    array.push(array[i - 1] + array[i]);
+  }
 
+  return array[array.length - 1];
 };
 
 // Do not edit below this line
